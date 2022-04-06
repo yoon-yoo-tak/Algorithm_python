63b101087b0282bcc15cf639872dcb73b154ed76
yoon-yoo-tak <dbsdbxkrzz@naver.com>
2022-04-06 23:25:02 +0900

    22.04.06
    test

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,32 +1,6 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
-def dfs(x, y):
-    visit[x][y] = True
-    for dx, dy in dxy:
-        nx, ny = x + dx, y + dy
-        if nx < 0 or nx >= n or ny < 0 or ny >=m:
-            continue
-        if graph[nx][ny] == 0:
-            continue
-        if visit[nx][ny]:
-            continue
-        dfs(nx, ny)
-
-
-for _ in range(int(input())):
-    m, n, k = map(int, input().split())
-    graph = [[0] * m for _ in range(n)]
-    visit = [[False] * m for _ in range(n)]
-    dxy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
-    for _ in range(k):
-        a, b = map(int, input().split())
-        graph[b][a] = 1
-    count = 0
-    for i in range(n):
-        for j in range(m):
-            if graph[i][j] == 1 and not visit[i][j]:
-                dfs(i, j)
-                count += 1
-    print(count)
+for i in range(0, 10, 2):
+    print(i, i + 1)
