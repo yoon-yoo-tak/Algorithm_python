a03f6a9a2eceacc3d7cc6d161c9317cf20705638
Your Name <dbsdbxkrzz@naver.com>
2022-04-10 23:27:26 +0900

    22.04.10
    reView

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,21 +1,40 @@
+"""
+16973 직사각형 탈출
+"""
 import sys
-from itertools import permutations
+from collections import deque
 input = sys.stdin.readline
 
+def bfs(x, y):
+    q = deque()
+    q.append((x, y))
+    dist[x][y] = 0
+    while q:
+        x, y = q.popleft()
+        for dx, dy in dxy:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < m and 0 <= nx + h - 1 < n and 0 <= ny + w - 1 < m:
+                if dist[nx][ny] == -1:
+                    if isOk(nx, ny):
+                        dist[nx][ny] = dist[x][y] + 1
+                        q.append((nx, ny))
+
+def isOk(x, y):
+    for xWall, yWall in wall:
+        if x <= xWall < x + h and y <= yWall < y + w:
+            return False
+    return True
+
 n, m = map(int, input().split())
-cream = [[False for _ in range(n)] for _ in range(n)]
-for i in range(m):
-    a, b = map(int, input().split())
-    cream[a - 1][b - 1] = True
-    cream[b - 1][a - 1] = True
 
-ans = 0
+graph = [list(map(int, input().split())) for _ in range(n)]
+dxy = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+h, w, sx, sy, ex, ey = map(int, input().split())
+dist = [[-1] * m for _ in range(n)]
+wall = [(i, j) for i in range(n) for j in range(m) if graph[i][j] == 1]
 
-for i in range(n):
-    for j in range(i + 1, n):
-        for k in range(j + 1, n):
-            if not cream[i][j] and not cream[i][k] and not cream[j][k]:
-                ans += 1
+bfs(sx - 1, sy - 1)
+print(dist[ex - 1][ey - 1])
 
-print(ans)
+
 
