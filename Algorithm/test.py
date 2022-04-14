7be347b35da6d86a1ea45f5242a23e4fae7672d6
Your Name <dbsdbxkrzz@naver.com>
2022-04-14 21:49:23 +0900

    22.04.14
    swea 문제풀이

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,2 +1,6 @@
-s = input()
-print(''.join(sorted(set(s))))
+t = int(input())
+for i in range(1, t + 1):
+    n = int(input())
+    ls = sorted(map(int, input().split()))
+    print(f'#{i}',end=' ')
+    print(*ls)
